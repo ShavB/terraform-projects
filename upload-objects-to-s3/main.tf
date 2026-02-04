e5e6ebe8f83ad19b5373260d84be33dc5c7a142e
Shyam Bisht <bishtshyam135@gmail.com>
2026-02-04 17:12:08 +0000

    multi object upload

--- upload-objects-to-s3/main.tf
+++ upload-objects-to-s3/main.tf
@@ -12,9 +12,12 @@ resource "aws_s3_bucket" "objects-folder" {
 }
 
 // Upload object to S3 bucket
+// Use for_each to upload multiple files 
 resource "aws_s3_object" "object" {
+  for_each = fileset("${path.module}/www/", "*")
+
   bucket = aws_s3_bucket.objects-folder.id
-  key    = "index-one.txt"
-  source = "${path.module}/www/index.txt"
-  etag   = filemd5("${path.module}/www/index.txt")
+  key    = each.value
+  source = "${path.module}/www/${each.value}"
+  etag   = filemd5("${path.module}/www/${each.value}")
 }
---
+++ upload-objects-to-s3/terraform.tfvars
@@ -0,0 +1,2 @@
+regions     = "eu-west-1"
+bucket_name = "shav-object-store-12345"
